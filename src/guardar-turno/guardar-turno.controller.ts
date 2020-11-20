f6a8ed2af6e0f75415d70f64107afa9946d88582
Karen Simari <simarikaren@gmail.com>
2020-11-20 12:05:44 -0300

    w

--- src/guardar-turno/guardar-turno.controller.ts
+++ src/guardar-turno/guardar-turno.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
-import { Delete } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { Delete, Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
 import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
 import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 import { GuardarTurnoService } from './guardar-turno.service';
