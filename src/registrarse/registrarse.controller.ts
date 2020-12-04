e38ed6d53393e1c276b09162a352031350a7285f
Karen Simari <simarikaren@gmail.com>
2020-12-04 14:18:24 -0300

    m

--- src/registrarse/registrarse.controller.ts
+++ src/registrarse/registrarse.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { Post } from '@nestjs/common/decorators/http/request-mapping.decorator';
-import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Body, Param } from '@nestjs/common/decorators/http/route-params.decorator';
 import { get } from 'http';
 import { RegistrarseService } from './registrarse.service';
 import { Registro } from './Registro';
@@ -10,14 +10,14 @@ export class RegistrarseController {
 
     constructor(private registrarseService: RegistrarseService){};
 
-    // @Get()
-    // public loadInfo(): Registro[] {
-    //     return this.registrarseService.loadInfo();
-    // }
-
     @Post()
     create(@Body() registro: any): string {
         return this.registrarseService.create(registro);
+    }
+
+    @Get(':usuario')
+    public getDatosUsuarioRegistrado(@Param('usuario') usuario: string) {
+        return this.registrarseService.getDatosUsuarioRegistrado(usuario);
     }
 }
 
